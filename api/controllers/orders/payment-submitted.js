f1f67e2fac0e07525210f7e8f583da6e9dc18f66
Jordan Kabyemela <wyzig34@gmail.com>
2021-01-14 19:33:18 +0000

    Payment stuff

--- api/controllers/orders/payment-submitted.js
+++ api/controllers/orders/payment-submitted.js
@@ -28,7 +28,7 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     var request = require('request-json');
-    var client = request.createClient('https://studio.fuse.io/api/v2/jobs/');
+    var client = request.createClient('https://studio.fuse.io/api/v2/');
 
     // var options = {
     //   'method': 'GET',
@@ -41,23 +41,39 @@ module.exports = {
 
     client.headers['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50QWRkcmVzcyI6IjB4NzU1QzY2MTcxNDkzMUIxQWM5MDRhOGVjYkRENzYxMDY4OUQ4MTJBYyIsImlzQ29tbXVuaXR5QWRtaW4iOnRydWUsImFwcE5hbWUiOiJSb29zdCIsImlhdCI6MTYwNTc5NzExNX0.ozqwhxFjTYUUoexnkBOY_TsW4sL_574gWZHqzHhD8Pk";
 
-    client.get(inputs.jobId)
+    client.get('jobs/' + inputs.jobId)
     .then(function(result){
       var paymentTotal = result.body.data.data.amount/(10**16); // in pence
       var paymentSenderWallet = result.body.data.data.wallet;
 
       Order.findOne(inputs.orderId)
       .then(function(order){
-        if (order.total == paymentTotal /*&& order.customerWallet == paymentSenderWallet && !order.paidDateTime*/) {
+        // TODO: check transaction using GBPx token
+        if (order.total == paymentTotal && order.customerWallet == paymentSenderWallet && !order.paidDateTime) {
           var unixtime = new Date().getTime();
           Order.updateOne(inputs.orderId)
           .set({
             paymentJobId: inputs.jobId,
             paidDateTime: unixtime
           }).then(function(success){
-            sails.sockets.broadcast('order'+order.id, 'paid', {orderId: order.id, paidDateTime: unixtime});
-            return exits.success(result.body);
-          }) //TODO: error catch
+            sails.sockets.broadcast('order' + order.id, 'paid', {orderId: order.id, paidDateTime: unixtime});
+            //https://studio.fuse.io/api/v2/admin/tokens/transfer
+
+            var rewardAmount = order.total * 10;
+
+            var data = {
+              tokenAddress: "0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4",
+              networkType: "fuse",
+              amount: rewardAmount,
+              from: "0x29249e06e8D3e4933cc403AB73136e698a08c38b",
+              to: order.customerWallet
+            }
+
+            client.post('admin/tokens/transfer', data)
+            .then(function(res){
+              return exits.success(result.body);
+            })
+          }) //TODO: error handling
         }
         else {
           return exits.error();
