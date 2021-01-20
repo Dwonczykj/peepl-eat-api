38254bda18c591621d0b21d4c0ff2a2bb529715c
adamlaunchcode <adam@launchcode.xyz>
2021-01-20 15:35:52 +0000

    Store user details

--- api/controllers/orders/view-order-confirmation.js
+++ api/controllers/orders/view-order-confirmation.js
@@ -24,8 +24,12 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    var order = await Order.findOne(inputs.orderId);
+    var order = await Order.findOne(inputs.orderId)
+    .populate('items.product&items.deliveryMethod&items.deliverySlot&items.optionValues.option&items.optionValues.optionValue')
 
+
+    console.log(order);
+    
     // Respond with view.
     return exits.success({
       order
