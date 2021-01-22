5377b72fa0b42f96c264d55553d463a9393dbaa0
adamlaunchcode <adam@launchcode.xyz>
2021-01-22 22:32:47 +0000

    Payment verification

--- api/controllers/orders/payment-submitted.js
+++ api/controllers/orders/payment-submitted.js
@@ -20,7 +20,7 @@ module.exports = {
     var client = request.createClient('https://studio.fuse.io/api/v2/');
     client.headers['Authorization'] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50QWRkcmVzcyI6IjB4NzU1QzY2MTcxNDkzMUIxQWM5MDRhOGVjYkRENzYxMDY4OUQ4MTJBYyIsImlzQ29tbXVuaXR5QWRtaW4iOnRydWUsImFwcE5hbWUiOiJSb29zdCIsImlhdCI6MTYwNTc5NzExNX0.ozqwhxFjTYUUoexnkBOY_TsW4sL_574gWZHqzHhD8Pk";
 
-    function makeQuery(iteration){
+    function makeQuery(iteration = 0){
       // TODO: Use setInterval or similar to repeat request every second until either success or failure (or timeout)
       setTimeout(function(){
         console.log("Checking transaction. #"+iteration);
