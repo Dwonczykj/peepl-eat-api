180449eb0fe389c83cd0d4d8215a4ef21b6e82cc
adamlaunchcode <adam@launchcode.xyz>
2021-01-29 14:06:23 +0000

    Print wallets

--- api/policies/has-wallet.js
+++ api/policies/has-wallet.js
@@ -13,6 +13,7 @@ module.exports = async function (req, res, proceed) {
     return res.redirect('/');
   } else {
     req.session.walletId = req.param('wallet') || req.session.walletId;
+    console.log(req.session.walletId);
     return proceed();
   }
 };
