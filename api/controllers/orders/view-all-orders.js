0e3dfacbd8f78db2f819482f42b1af5a6bf2f1a6
adamlaunchcode <adam@launchcode.xyz>
2021-01-29 14:56:45 +0000

    Basic 'all orders' screen

--- api/controllers/orders/view-all-orders.js
+++ api/controllers/orders/view-all-orders.js
@@ -18,7 +18,7 @@ module.exports = {
 
   fn: async function () {
     var orders = await Order.find({paidDateTime: {'>': 0}})
-    .populate('items.product&deliveryMethod&deliverySlot&optionValues&optionValues.option&optionValue');
+    .populate('items.product&deliveryMethod&deliverySlot&optionValues&optionValues.option&optionValue')
 
     // Respond with view.
     return {orders};
