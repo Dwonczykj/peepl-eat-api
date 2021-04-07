91ceb20f6b9ff0736688baa3d243eeef91d6e70e
Adam Galloway <adam@launchcode.xyz>
2021-04-07 15:40:51 +0100

    Hotfix

--- api/controllers/orders/view-my-orders.ts
+++ api/controllers/orders/view-my-orders.ts
@@ -13,12 +13,12 @@ module.exports = {
 
   },
 
-  fn: async function () {
+  fn: async function (inputs, exits) {
     var orders = await Order.find({customer: this.req.session.walletId, paidDateTime: {'>': 0}})
     .populate('items.product&deliveryMethod&deliverySlot&optionValues&optionValues.option&optionValue');
 
     // Respond with view.
-    return {orders};
+    return exits.success({orders});
 
   }
 
