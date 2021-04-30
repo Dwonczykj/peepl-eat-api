63e65c0cdc34106d1e904f5d6e36f1f8f54a9674
Adam Galloway <adam@launchcode.xyz>
2021-04-30 13:38:00 +0100

    Edit vendor page changes

--- assets/js/pages/admin/edit-vendor.page.js
+++ assets/js/pages/admin/edit-vendor.page.js
@@ -6,20 +6,37 @@ parasails.registerPage('admin-edit-vendor', {
     syncing: false,
     cloudError: '',
     formErrors: {
-      name: '',
-      description: '',
-      image: '',
-      type: '',
-      walletId: ''
     },
     imageName: 'Choose image',
     vendor: {
       name: '',
       description: '',
       image: '',
-      type: '',
-      walletId: '',
-    }
+      type: 'restaurant',
+      walletId: '0x',
+      deliveryRestrictionDetails: '',
+      status: 'draft',
+    },
+    formRules: {
+      name: {
+        required: true
+      },
+      description: {
+        maxLength: 400,
+        required: true
+      },
+      type: {
+      },
+      deliveryRestrictionDetails: {
+        required: false
+      },
+      walletId: {
+        required: true,
+        maxLength: 100
+      },
+      status: {
+      },
+    },
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -29,6 +46,7 @@ parasails.registerPage('admin-edit-vendor', {
   },
   mounted: async function() {
     _.extend(this, SAILS_LOCALS);
+    this.vendor.image = "";
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -36,7 +54,7 @@ parasails.registerPage('admin-edit-vendor', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
     changeVendorImageInput: function(files) {
-      if (files.length !== 1 && !this.updatedVendor.photo) {
+      if (files.length !== 1 && !this.vendor.image) {
         throw new Error('Consistency violation: `changeFileInput` was somehow called with an empty array of files, or with more than one file in the array!  This should never happen unless there is already an uploaded file tracked.');
       }
       var selectedFile = files[0];
@@ -44,13 +62,16 @@ parasails.registerPage('admin-edit-vendor', {
       // If you cancel from the native upload window when you already
       // have a photo tracked, then we just avast (return early).
       // In this case, we just leave whatever you had there before.
-      if (!selectedFile && this.updatedVendor.photo) {
+      if (!selectedFile && this.vendor.image) {
         return;
       }
 
-      this.imageName = selectedFile.name;
-      this.updatedVendor.image = selectedFile;
+      this.imageName = selectedFile.name; // Used to show user which image is selected
+      this.vendor.image = selectedFile;
       this.formErrors.image = '';
     },
-  }
+    vendorSubmitted: function({id}) {
+      window.location.href = '/admin/vendors/' + id;
+    }
+  },
 });
