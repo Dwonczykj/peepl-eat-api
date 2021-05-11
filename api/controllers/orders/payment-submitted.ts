e398a9dbca6e5cc4232a9820dcf721c7fb743957
Adam Galloway <adam@launchcode.xyz>
2021-05-11 18:23:43 +0100

    Better payment handling

--- api/controllers/orders/payment-submitted.ts
+++ api/controllers/orders/payment-submitted.ts
@@ -17,137 +17,136 @@ module.exports = {
   exits: {
   },
   fn: async function (inputs, exits) {
-    var request = require('request-json');
-    var client = request.createClient('https://studio.fuse.io/api/v2/');
-    const { IncomingWebhook } = require('@slack/webhook');
+    var axios = require('axios');
 
-    client.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50QWRkcmVzcyI6IjB4NzU1QzY2MTcxNDkzMUIxQWM5MDRhOGVjYkRENzYxMDY4OUQ4MTJBYyIsImlzQ29tbXVuaXR5QWRtaW4iOnRydWUsImFwcE5hbWUiOiJSb29zdCIsImlhdCI6MTYwNTc5NzExNX0.ozqwhxFjTYUUoexnkBOY_TsW4sL_574gWZHqzHhD8Pk';
-    const url = 'https://hooks.slack.com/services/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';
-    const webhook = new IncomingWebhook(url);
+    var client = axios.create({
+      baseURL: sails.config.custom.fuseStudioBaseUrl,
+      timeout: 1000,
+      headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50QWRkcmVzcyI6IjB4NzU1QzY2MTcxNDkzMUIxQWM5MDRhOGVjYkRENzYxMDY4OUQ4MTJBYyIsImlzQ29tbXVuaXR5QWRtaW4iOnRydWUsImFwcE5hbWUiOiJSb29zdCIsImlhdCI6MTYwNTc5NzExNX0.ozqwhxFjTYUUoexnkBOY_TsW4sL_574gWZHqzHhD8Pk'}
+    });
 
-
+    // Recursive function - runs every second until payment fails or succeeds (30s timeout)
     function makeQuery(iteration = 0){
-      setTimeout(() => {
-        console.log('Checking transaction id: ' + inputs.jobId + ' Attempt#: ' + iteration);
-        client.get('jobs/' + inputs.jobId)
-        .then((result) => {
-          var paymentInfo = result.body.data.data.transactionBody;
+      setTimeout(async () => {
+        sails.log('Checking transaction id: ' + inputs.jobId + ' Attempt #: ' + iteration);
 
-          var paymentTotal = paymentInfo.value/(10**16); // in pence
-          var paymentSenderWallet = paymentInfo.from;
-          var paymentRecipientWallet = paymentInfo.to;
-          var paymentTokenAddress = paymentInfo.tokenAddress;
-          var paymentStatus = paymentInfo.status;
-          var transactionType = paymentInfo.type;
+        // Make request to Fuse API to check payment status
+        var jobDetails = await client.get('/jobs/' + inputs.jobId);
 
-          if(iteration >= 29){
-            // Timeout if taken longer than 30 seconds
-            return;
+        var paymentInfo = jobDetails.data.data.data.transactionBody;
+
+        var paymentTotal = paymentInfo.value/(10**16); // in pence
+        var paymentSenderWallet = paymentInfo.from;
+        var paymentRecipientWallet = paymentInfo.to;
+        var paymentTokenAddress = paymentInfo.tokenAddress;
+        var paymentStatus = paymentInfo.status;
+        var transactionType = paymentInfo.type;
+
+        if(iteration >= 29){
+          // Timeout if taken longer than 30 seconds
+          return;
+        }
+
+        if(paymentStatus === 'pending'){
+          return makeQuery(iteration+1);
+        }
+
+        var orderDetails = await Order.findOne(inputs.orderId)
+        .populate('items.product&deliveryMethod&deliverySlot&optionValues&optionValues.option&optionValue&vendor');
+
+        var error = '';
+
+        if(!orderDetails){
+          // Order doesn't exist
+          return exits.error({message: 'Order doesn\'t exist!'});
+        } else {
+          // Validations
+          // Incorrect Recipient
+          if(paymentRecipientWallet !== orderDetails.items[0].product.vendor.walletId) {
+            error = 'Recipient doesn\'t match!';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
+          }
+          // Incorrect Currency
+          if(paymentTokenAddress.toUpperCase() !== '0x40AFCD9421577407ABB0d82E2fF25Fd2Ef4c68BD'.toUpperCase()) {//GBPX token address
+            error = 'Incorrect token sent.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
+          }
+          // Incorrect Sender
+          if (paymentSenderWallet !== orderDetails.customer){
+            error = 'Transaction sender is not customer.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
+          }
+          // Incorrect Total
+          if(paymentTotal !== orderDetails.total){
+            error =  'Transaction amount doesn\'t match order total.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
+          }
+          // Incorrect Status
+          if(paymentStatus !== 'confirmed'){
+            error = 'Transaction failed. Check wallet balance.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
+          }
+          // Incorrect Transaction Type
+          if (transactionType !== 'SEND'){
+            error = 'Incorrect transaction type.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
           }
 
-          if(paymentStatus == 'pending'){
-            return makeQuery(iteration+1);
+          // Transaction already been used (replay attack)
+          var existingOrdersWithJobId = await Order.findOne({paymentJobId: inputs.jobId});
+          if(existingOrdersWithJobId){
+            error = 'Transaction has already been used for another order.';
+            sails.sockets.broadcast('order' + orderDetails.id, 'paymentError', {orderId: orderDetails.id, message: error});
+            return exits.error({message: error});
           }
 
-          Order.findOne(inputs.orderId)
-          .populate('items.product.vendor')
-          .then(async (order) => {
-            var error = '';
+          var unixtime = Date.now();
 
-            if(!order){
-              return exits.error();
-            }else {
-              if(paymentRecipientWallet != order.items[0].product.vendor.walletId) {
-                error = 'Recipient doesn\'t match!';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-              if(paymentTokenAddress.toUpperCase() != '0x40AFCD9421577407ABB0d82E2fF25Fd2Ef4c68BD'.toUpperCase()) {//GBPX token address
-                error = 'Incorrect token sent.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-              if (paymentSenderWallet != order.customer){
-                error = 'Transaction sender is not customer.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-              if(paymentTotal != order.total){
-                error =  'Transaction amount doesn\'t match order total.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-              if(paymentStatus != 'confirmed'){
-                error = 'Transaction failed. Check wallet balance.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-              if (transactionType != 'SEND'){
-                error = 'Incorrect transaction type.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-
-              var existingOrdersWithJobId = await Order.findOne({paymentJobId: inputs.jobId});
-              if(existingOrdersWithJobId){
-                error = 'Transaction has already been used for another order.';
-                sails.sockets.broadcast('order' + order.id, 'paymentError', {orderId: order.id, message: error});
-                return exits.error({message: error});
-              }
-
-              var unixtime = Date.now();
-              Order.updateOne(inputs.orderId)
-              .set({
-                paymentJobId: inputs.jobId,
-                paidDateTime: unixtime
-              }).then(() => {
-                // Notify client of successful transaction
-                sails.sockets.broadcast('order' + order.id, 'paid', {orderId: order.id, paidDateTime: unixtime});
-
-                if(sails.config.environment === 'production'){
-                  (async () => {
-                    await webhook.send({
-                      text: `New order created #${order.id} for £${order.total / 100}! Check it out: https://app.itsaboutpeepl.com/orders/${order.id}`,
-                    });
-
-                    // Issue PPL token reward
-                    var rewardAmount = order.total; // 10 PPL tokens = £0.01, reward is 10% of spend
-
-                    var data = {
-                      tokenAddress: '0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4',
-                      networkType: 'fuse',
-                      amount: rewardAmount.toString(),
-                      from: '0x29249e06e8D3e4933cc403AB73136e698a08c38b',
-                      to: order.customer
-                    };
-
-                    client.post('admin/tokens/transfer', data);
-
-                    // TODO: remove redundant query
-                    Order.findOne(inputs.orderId)
-                    .populate('items.product&deliveryMethod&deliverySlot&optionValues&optionValues.option&optionValue&vendor')
-                    .then(async (fullOrder) => {
-                      await sails.helpers.sendTemplateEmail.with({
-                        template: 'email-order-confirmation-new',
-                        templateData: {order: fullOrder},
-                        to: fullOrder.deliveryEmail,
-                        subject: 'Peepl Eat Order Confirmed - #' + fullOrder.id ,
-                        layout: false,
-                      });
-                      // TODO: Mailchimp add completed_order event - NOT created_order event!
-                    });
-                  })();
-                }
-
-                return exits.success(result.body);
-              });
-            }
+          // Update order with payment ID and time
+          await Order.updateOne(inputs.orderId)
+          .set({
+            paymentJobId: inputs.jobId,
+            paidDateTime: unixtime
           });
 
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+          // Notify subscribed sockets of order status
+          sails.sockets.broadcast('order' + orderDetails.id, 'paid', {orderId: orderDetails.id, paidDateTime: unixtime});
+
+          if(sails.config.environment === 'production'){
+            // Send slack notification to orders channel
+            await axios.post(sails.config.custom.slackOrdersWebhook, {
+              text: `[TEST] New order created #1 for £0! Check it out: https://app.itsaboutpeepl.com/orders/1`,
+            });
+
+            // Issue PPL token reward
+            var rewardAmount = orderDetails.total; // 10 PPL tokens = £0.01, reward is 10% of spend
+            var data = {
+              tokenAddress: '0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4',
+              networkType: 'fuse',
+              amount: rewardAmount.toString(),
+              from: '0x29249e06e8D3e4933cc403AB73136e698a08c38b',
+              to: orderDetails.customer
+            };
+            await client.post('admin/tokens/transfer', data);
+
+            // Send order confirmation email
+            await sails.helpers.sendTemplateEmail.with({
+              template: 'email-order-confirmation-new',
+              templateData: {order: orderDetails},
+              to: orderDetails.deliveryEmail,
+              subject: 'Peepl Eat Order Confirmed - #' + orderDetails.id ,
+              layout: false,
+            });
+          }
+
+          return exits.success(jobDetails.body);
+        }
       }, 1000);
     }
 
--- config/custom.js
+++ config/custom.js
@@ -9,18 +9,15 @@
  */
 
 module.exports.custom = {
-
   /***************************************************************************
   *                                                                          *
   * Any other custom config this Sails app should use during development.    *
   *                                                                          *
   ***************************************************************************/
-  // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
-  // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
-  // …
   amazonSesAccessKeyId: '',
   amazonSesAccessKeySecret: '',
   amazonSesRegion: 'eu-west-1',
   mailchimpAPIKey: '53745999cd9909b57e46c059db39c947',
-
+  fuseStudioBaseUrl: 'https://studio.fuse.io/api/v2/',
+  slackOrdersWebhook: 'https://hooks.slack.com/services/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'
 };
