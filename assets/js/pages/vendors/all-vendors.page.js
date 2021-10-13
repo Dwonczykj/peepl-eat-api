2d15cab82189a666ce2b58dbfc0a6e2d43226b73
Adam Galloway <adam@launchcode.xyz>
2021-10-13 11:15:54 +0100

    Payment handler test

--- assets/js/pages/vendors/all-vendors.page.js
+++ assets/js/pages/vendors/all-vendors.page.js
@@ -21,6 +21,16 @@ parasails.registerPage('all-vendors', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
+    clickS: function() {
+      var paymentDetails = {
+        action: 'pay',
+        amount: 20, //Pence to pounds
+        currency: 'GBPX',
+        destination: '0f2e73f69943A25c13b7d4821A6b82e611ca3E8308',
+        orderId: 1
+      };
+
+      window.flutter_inappwebview.callHandler('pay', paymentDetails);
+    }
   }
 });
