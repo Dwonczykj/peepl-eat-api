76405201bf0bb3c6ed7bc67363f53bbcbb5b5304
Adam Galloway <adam@launchcode.xyz>
2022-06-15 15:39:11 +0100

    Updated CRUD endpoints

--- api/controllers/admin/create-product.ts
+++ api/controllers/admin/create-product.ts
@@ -33,6 +33,9 @@ module.exports = {
     priority: {
       type: 'number'
     },
+    isFeatured: {
+      type: 'boolean'
+    },
     vendor: {
       type: 'number'
     }
--- api/controllers/admin/edit-product.ts
+++ api/controllers/admin/edit-product.ts
@@ -29,6 +29,9 @@ module.exports = {
     },
     priority: {
       type: 'number'
+    },
+    isFeatured: {
+      type: 'boolean'
     }
   },
 
