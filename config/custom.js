1d1ad9a2b356ee785fce15d01d5033febe9ab20f
Adam Galloway <adam@launchcode.xyz>
2022-07-27 12:22:47 +0100

    Secrets Management

--- config/custom.js
+++ config/custom.js
@@ -14,21 +14,15 @@ module.exports.custom = {
   * Any other custom config this Sails app should use during development.    *
   *                                                                          *
   ***************************************************************************/
-  baseUrl: 'http://localhost:1337',
-  passwordSaltRounds: 10,
+  baseUrl: 'https://vegi.itsaboutpeepl.com',
+  internalEmailAddress: 'support@itsaboutpeepl.com',
+  buildNumber: 1,
+  peeplWebhookAddress: 'https://vegi.itsaboutpeepl.com/api/v1/orders/peepl-pay-webhook',
   fuseStudioBaseUrl: 'https://studio.fuse.io/api/v2/',
   pplTokenAddress: '0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4',
   pplRewardsPoolAddress: '0x29249e06e8D3e4933cc403AB73136e698a08c38b',
   coopcycleUrl: 'https://agile-delivery.coopcycle.org',
   peeplPayUrl: 'https://pay.itsaboutpeepl.com/api/v1',
-  peeplWebhookAddress: 'http://localhost:1337/api/v1/orders/peepl-pay-webhook',
   amazonS3BucketUrl: 'https://vegiapp-1.s3.us-east-1.amazonaws.com/',
-  amazonS3Secret: 'Ob+Z6nSxSLa95NSKheFAXPMT2OYzdKOMCuVmd52Q',
   amazonS3Bucket: 'vegiapp-1',
-  amazonS3AccessKey: 'AKIA57YJIRDKCR42R7MU',
-  mailchimpAPIKey: '',
-  mailchimpListId: '',
-  peeplAPIKey: 'Oko4WFgwUFEtVlI2NE01Ry1QTkVBMzFZLVlQVllRQ1o=',
-  twilioSID: 'AC2bc72aa996c7a9f5b33771c1135e0261',
-  twilioAuthToken: '496e6e1846ad69163857ea3d7fa347e4',
 };
--- config/env/production.js
+++ config/env/production.js
@@ -369,20 +369,13 @@ module.exports = {
     internalEmailAddress: 'support@itsaboutpeepl.com',
     buildNumber: 1,
     peeplWebhookAddress: 'https://vegi.itsaboutpeepl.com/api/v1/orders/peepl-pay-webhook',
-
-    // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
-    // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
-    //--------------------------------------------------------------------------
-    // /\   OR, to avoid checking them in to version control, you might opt to
-    // ||   set sensitive credentials like these using environment variables.
-    //
-    // For example:
-    // ```
-    // sendgridSecret=SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU
-    // sails_custom__stripeSecret=sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm
-    // ```
-    //--------------------------------------------------------------------------
-
+    fuseStudioBaseUrl: 'https://studio.fuse.io/api/v2/',
+    pplTokenAddress: '0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4',
+    pplRewardsPoolAddress: '0x29249e06e8D3e4933cc403AB73136e698a08c38b',
+    coopcycleUrl: 'https://agile-delivery.coopcycle.org',
+    peeplPayUrl: 'https://pay.itsaboutpeepl.com/api/v1',
+    amazonS3BucketUrl: 'https://vegiapp-1.s3.us-east-1.amazonaws.com/',
+    amazonS3Bucket: 'vegiapp-1',
   },
 
   uploads: {
