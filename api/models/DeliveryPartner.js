f4b911922a6c2837935178ab622f9f816a1271a2
Adam Galloway <adam@launchcode.xyz>
2022-08-01 16:00:03 +0100

    Delivery Partner Model

---
+++ api/models/DeliveryPartner.js
@@ -0,0 +1,54 @@
+/**
+ * DeliveryPartner.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: 'string',
+      description: 'The name of the Delivery Partner.',
+      required: true,
+      maxLength: 50
+    },
+    email:{
+      type: 'string',
+      description: 'The email of the Delivery Partner.',
+      required: true,
+      isEmail: true,
+      maxLength: 50
+    },
+    phoneNumber: {
+      type: 'string',
+      description: 'Phone number of the Delivery Partner.',
+      required: true
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    fulfilmentMethod: {
+      model: 'FulfilmentMethod',
+    }
+  },
+
+  afterCreate: async function (newlyCreatedRecord, proceed) {
+    await sails.helpers.initialiseDeliveryMethods(newlyCreatedRecord.id, 'vendor');
+
+    return proceed();
+  }
+
+};
+
