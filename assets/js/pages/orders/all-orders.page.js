1e0bd80c9804e4fda46e1ccd764112526663c671
Adam Galloway <adam@launchcode.xyz>
2022-08-22 15:42:15 +0100

    All orders page - added filters

--- assets/js/pages/orders/all-orders.page.js
+++ assets/js/pages/orders/all-orders.page.js
@@ -4,6 +4,8 @@ parasails.registerPage('all-orders', {
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
     //…
+    timePeriod: '',
+    acceptanceStatus: '',
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
