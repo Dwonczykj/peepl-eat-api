ea97bc7252da9ba28e2063b096bebddf4da1073a
Adam Galloway <adam@launchcode.xyz>
2022-08-29 23:50:15 +0100

    Charge peepl rewards change

--- api/helpers/issue-peepl-reward.js
+++ api/helpers/issue-peepl-reward.js
@@ -32,8 +32,8 @@ module.exports = {
   fn: async function (inputs) {
     var client = axios.create({
       baseURL: sails.config.custom.fuseStudioBaseUrl,
-      timeout: 1000,
-      headers: {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50QWRkcmVzcyI6IjB4NzU1QzY2MTcxNDkzMUIxQWM5MDRhOGVjYkRENzYxMDY4OUQ4MTJBYyIsImlzQ29tbXVuaXR5QWRtaW4iOnRydWUsImFwcE5hbWUiOiJSb29zdCIsImlhdCI6MTYwNTc5NzExNX0.ozqwhxFjTYUUoexnkBOY_TsW4sL_574gWZHqzHhD8Pk'}
+      timeout: 20000,
+      headers: {'API-SECRET': 'sk_q8TiAxOSniziV6G4CPHY1Ql1'}
     });
 
     // Issue PPL token reward
@@ -45,7 +45,11 @@ module.exports = {
       to: inputs.recipient
     };
 
-    await client.post('admin/tokens/transfer', data);
+    await client.post('admin/tokens/transfer?apiKey=pk_FDM9_lyd9leeOG4utgmzfr6h', data)
+    .catch((err) => {
+      sails.log.error(err);
+      throw new Error('Error issuing PPL token reward.');
+    });
 
     return;
   }
--- config/custom.js
+++ config/custom.js
@@ -18,7 +18,7 @@ module.exports.custom = {
   internalEmailAddress: 'support@itsaboutpeepl.com',
   buildNumber: 1,
   peeplWebhookAddress: 'https://vegi.itsaboutpeepl.com/api/v1/orders/peepl-pay-webhook',
-  fuseStudioBaseUrl: 'https://studio.fuse.io/api/v2/',
+  fuseStudioBaseUrl: 'https://api.chargeweb3.com/api/v0/',
   pplTokenAddress: '0xa2C7CdB72d177f6259cD12a9A06Fdfd9625419D4',
   pplRewardsPoolAddress: '0x29249e06e8D3e4933cc403AB73136e698a08c38b',
   coopcycleUrl: 'https://agile-delivery.coopcycle.org',
