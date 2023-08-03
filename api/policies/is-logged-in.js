828b1e125fe878ed1d69c9d7b8dd2b13a71bc222
Dwonczykj <jdwonczyk@gmail.com>
2023-08-03 11:12:39 +0100

    Logout stale user sessions

--- api/policies/is-logged-in.js
+++ api/policies/is-logged-in.js
@@ -8,6 +8,23 @@
  *   https://sailsjs.com/docs/concepts/policies/access-control-and-permissions
  */
 module.exports = async function (req, res, proceed) {
+  try {
+    if (req && (req.method === 'POST' || req.method === 'PUT') && req.body) {
+      if (req.url.includes('logging/log')) {
+        sails.log.silly(`🔗➡️ REQUEST [${req.method}]: "${req.url}"`);
+      } else {
+        sails.log.verbose(
+          `🔗➡️ REQUEST [${req.method}]: "${req.url}" body: ${JSON.stringify(
+            req.body
+          )}`
+        );
+      }
+    } else {
+      sails.log.verbose(`🔗➡️ REQUEST [${req.method}]: "${req.url}"`);
+    }
+  } catch (err) {
+    sails.log.warn(err);
+  }
   if (!req.session.userId && req.headers['api-key'] && req.headers['api-secret']) {
     var serviceAccounts = await User.find({
       secret: req.headers['api-secret'],
@@ -30,23 +47,28 @@ module.exports = async function (req, res, proceed) {
       // ~ https://sailsjs.com/documentation/reference/request-req/req-original-url
       return res.redirect('/admin/login-with-password?next=' + encodeURIComponent(req.originalUrl));
     }
-  }
-  try {
-    if (req && (req.method === 'POST' || req.method === 'PUT') && req.body) {
-      if (req.url.includes('logging/log')) {
-        sails.log.silly(`🔗➡️ REQUEST [${req.method}]: "${req.url}"`);
-      } else {
-        sails.log.verbose(
-          `🔗➡️ REQUEST [${req.method}]: "${req.url}" body: ${JSON.stringify(
-            req.body
-          )}`
+  } else {
+    try {
+      let user = await User.findOne({ id: this.req.session.userId });
+      if(!user || user.length < 1){
+        sails.log(
+          'Policy:<is-logged-in> -> redirect to login as no active session'
+        );
+        // ~ https://sailsjs.com/documentation/reference/request-req/req-original-url
+        return res.redirect(
+          '/admin/login-with-password?next=' +
+            encodeURIComponent(req.originalUrl)
         );
       }
-    } else {
-      sails.log.verbose(`🔗➡️ REQUEST [${req.method}]: "${req.url}"`);
+    } catch (error) {
+      sails.log(
+        'Policy:<is-logged-in> -> redirect to login as no active session'
+      );
+      // ~ https://sailsjs.com/documentation/reference/request-req/req-original-url
+      return res.redirect(
+        '/admin/login-with-password?next=' + encodeURIComponent(req.originalUrl)
+      );
     }
-  } catch (err) {
-    sails.log.warn(err);
   }
   return proceed();
 };
